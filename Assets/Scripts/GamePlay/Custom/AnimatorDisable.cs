2ce153465bf2b5712a1efc13d9ea621ad6779740
Nikita Ustinov <122388971+NikitaUstinov092@users.noreply.github.com>
2023-10-23 22:00:22 +0300

    Рефакторинг

--- Assets/Scripts/GamePlay/Custom/AnimatorDisable.cs
+++ Assets/Scripts/GamePlay/Custom/AnimatorDisable.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimatorDisable : MonoBehaviour
 {
-   [SerializeField] private Animator _anim;
-
-
+    [SerializeField]
+    private Animator _anim;
+    
    public void DisableAnimator()
    {
       _anim.enabled = false;
